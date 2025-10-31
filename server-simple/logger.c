19dad8d7acea46015e0d40fb9baf26337eb26ab2
Jim Shortz <farx1024@outlook.com>
2025-10-31 16:22:34 -0400

    Cleaned up code.

--- server-simple/logger.c
+++ server-simple/logger.c
@@ -6,13 +6,18 @@
 #include <unistd.h>
 #include <mosquitto.h>
 
+/* 1 if the process should continue running */
 static int run = 1;
+
+/* File handle to log to */
 static FILE* log_fh = NULL;
 
+/* Configuration info */
 const char * mqtt_host;
 int mqtt_port;
 const char* mqtt_topic;
 
+/* getenv() with a default value */
 const char* getenv2(const char* name, const char* def) {
     const char* val;
     val = getenv(name);
@@ -26,28 +31,19 @@ void handle_signal(int s) {
     run = 0;
 }
 
-void connect_callback(struct mosquitto *mosq, void *obj, int result) {
-    fprintf(stderr, "Connected (status %d)\n", result);
-    if (result == 0) {
-	fprintf(stderr, "Subscribing to topic %s\n", mqtt_topic);
-	mosquitto_subscribe(mosq, NULL, mqtt_topic, 0);	
-    }
-}
-
-void disconnect_callback(struct mosquitto* mosq, void* obj, int result) {
-    fprintf(stderr, "Disconnected (status %d)\n", result);
-}
-
 void close_log_file() {
     if (log_fh)
 	fclose(log_fh);
+    
     log_fh = NULL;
 }
 
+/* Returns a file handle for the correct logfile for this timestamp */
 FILE* open_log_file(struct tm* tm) {
     static int last_day = -1;
     char fname[32];
-    
+
+    /* If it's the first time through or a new day, open a new file */
     if (log_fh == NULL || tm->tm_mday != last_day) {
 	close_log_file();
 
@@ -65,28 +61,43 @@ FILE* open_log_file(struct tm* tm) {
     return log_fh;
 }
 
-void message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_message *message) {
+void connect_callback(struct mosquitto *mosq, void *obj, int result) {
+    fprintf(stderr, "Connected (status %d)\n", result);
+    
+    if (result == 0) {
+	fprintf(stderr, "Subscribing to topic %s\n", mqtt_topic);
+	mosquitto_subscribe(mosq, NULL, mqtt_topic, 0);	
+    }
+}
+
+void disconnect_callback(struct mosquitto* mosq, void* obj, int result) {
+    fprintf(stderr, "Disconnected (status %d)\n", result);
+}
+
+
+/* Called when a message is received */
+void message_callback(struct mosquitto *mosq, void *obj,
+		      const struct mosquitto_message *message) {
     time_t now;
     char ts[32];
     FILE* f = NULL;
     struct tm* tm;
 
-    /* Format time in ISO format */
+    /* Capture the timestamp immediately */
     now = time(NULL);
     tm = gmtime(&now);
     strftime(ts, sizeof(ts), "%Y-%m-%dT%H:%M:%SZ", tm);
 
-    /* Write entry to rotated log file */
+    /* Write entry to log file */
     f = open_log_file(tm);
     if (fprintf(f, "%s,%s,%s\n", ts, message->topic, (char*)message->payload) < 0) {
-	perror("Error writing log entry");
-	exit(2);
+	perror("Error writing log entry, dropping datapoint.");
     }
     fflush(f);
 }
 
 int main(int argc, char *argv[]) {
-    char clientid[24];
+    char client_id[24];
     struct mosquitto *mosq;
     int rc = 0;
 
@@ -94,36 +105,37 @@ int main(int argc, char *argv[]) {
     mqtt_port = atoi(getenv2("MQTT_PORT", "1883"));
     mqtt_topic = getenv2("MQTT_TOPIC", "#");
 
-    printf("Starting logger host=%s port=%d topic=%s\n", mqtt_host, mqtt_port, mqtt_topic);
-
-    /*
-    signal(SIGINT, handle_signal);
-    signal(SIGTERM, handle_signal);
+    /* Are these actually needed?
+       signal(SIGINT, handle_signal);
+       signal(SIGTERM, handle_signal);
     */
 
     mosquitto_lib_init();
 
-    memset(clientid, 0, 24);
-    snprintf(clientid, 23, "simplog-%d", getpid());
-    mosq = mosquitto_new(clientid, true, NULL);
+    /* Generate a random client ID */
+    memset(client_id, 0, sizeof(client_id));
+    snprintf(client_id, sizeof(client_id)-1, "simplog-%x", rand() & 0xffff);
+    
+    mosq = mosquitto_new(client_id, true, NULL);
     
     if (mosq){
 	mosquitto_connect_callback_set(mosq, connect_callback);
 	mosquitto_disconnect_callback_set(mosq, disconnect_callback);
 	mosquitto_message_callback_set(mosq, message_callback);
 
-	printf("Connecting to MQTT host %s port %d\n", mqtt_host, mqtt_port);
+	fprintf(stderr, "Connecting to MQTT host %s port %d\n", mqtt_host, mqtt_port);
 	rc = mosquitto_connect(mosq, mqtt_host, mqtt_port, 60);
 	
 	while (run) {
 	    rc = mosquitto_loop(mosq, -1, 1);
+
 	    if (run && rc){
-		fprintf(stderr, "Error %d, Reconnecting in 20s\n", rc);
 		sleep(20);
 		fprintf(stderr, "Reconnecting\n");
 		mosquitto_reconnect(mosq);
 	    }
 	}
+	
 	mosquitto_destroy(mosq);
     } else {
 	perror("Error initializing MQTT");
